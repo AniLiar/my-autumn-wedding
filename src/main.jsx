788ef2ef54d40f74be560c0ee48e1b1a85d0e498
aniliar <annia.leonova@gmail.com>
2025-08-03 13:20:10 +0300

    changed urls

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter  basename="/my-autumn-wedding/">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
